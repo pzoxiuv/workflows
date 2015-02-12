adf31a4af34094ad353835dfa351ef934932cf24
Alex Merenstein <alex.merenstein@gmail.com>
2015-02-11 21:59:29 -0500

    Lots of stuff.
    - New control file format (see design and control_ex)
    - Adds subprocess calls and conditonals based on control file.
    - Replaces subprocess.call with subprocess.check_output.
    - Splits script output on spaces, puts output into vars based on workflow file.
    
    Maybe other stuff too.

--- orch.py
+++ orch.py
@@ -7,38 +7,70 @@ def prepend_boilerplate(f):
 	f.write("def main():\n")
 
 def append_boilerplate(f):
-	f.write("if __name__ == \"__main__\":\n")
+	f.write("\nif __name__ == \"__main__\":\n")
 	f.write("\tmain()")
 
-def add_block(f, block):
+def add_block(outfile, block, num_tabs):
 	# TODO: Add some checks, e.g. that script is a single word
 	args = shlex.split(block['arg_string'])
 	args.insert(0, block['script'])
-	print(args)
+	#print(args)
 	
-	subproc_str = "\tsubprocess.call(["
+	tabs= "\t"	# Starts with at least one tab because they're called from a method, newline for readability
+	for x in range(0, num_tabs):
+		tabs += "\t"
+
+	subproc_str = "\n" +tabs + "output = subprocess.check_output(["
 	for arg in args:
 		subproc_str += "\"" + arg + "\", "
 	subproc_str = subproc_str[:-2]	# Remove trailing comma and space
-	subproc_str += "])\n\n"
-	f.write(subproc_str)
-	outfile.close()
+	subproc_str += "])\n"
+	outfile.write(subproc_str)
 
-def main(wf_file, output_filename):
+	outfile.write(tabs + "print(output)\n")
+
+	outfile.write(tabs + "split_output = output.split(\" \")\n")
+	output_num = 0
+	for output in block['outputs']:
+		outfile.write(tabs + output + " = split_output[" + str(output_num) + "]\n")
+		output_num += 1
+
+def add_cond(outfile, cond, num_tabs):
+	cond_str = "\n\t"	# Starts with at least one tab because they're called from a method, newline for readability
+	for x in range(0, num_tabs):
+		cond_str += "\t"
+	cond_str += cond + "\n"
+
+	outfile.write(cond_str)
+
+def parse_control(outfile, control_file_data, blocks):
+	for line in control_file_data:
+		# XXX: Need to account for tabs on right side?  That'll mess this up
+		num_tabs = line.count('\t')
+		line = line.strip()
+		if line[0] == 'b':
+			add_block(outfile, blocks[int(line[1:])-1], num_tabs)
+		else:
+			add_cond(outfile, line, num_tabs)
+
+def main(wf_file, control_file, output_filename):
 	outfile = open(output_filename, 'w')
 	prepend_boilerplate(outfile)
 
 	with open(wf_file) as workflow_data:
 		workflow = json.loads(workflow_data.read())
 
-	for block in workflow['blocks']:
-		add_block(outfile, block)
-		print('id: ' + str(block['id']))
-		print('script: ' + block['script'])
-		print('arg_string: ' + block['arg_string'])
+	#for block in workflow['blocks']:
+	#	add_block(outfile, block)
+	#	print('id: ' + str(block['id']))
+	#	print('script: ' + block['script'])
+	#	print('arg_string: ' + block['arg_string'])
+
+	with open(control_file) as control_file_data:
+		parse_control(outfile, control_file_data, workflow['blocks'])
 
 	append_boilerplate(outfile)
 	outfile.close()
 
 if __name__ == "__main__":
-	main(sys.argv[1], 'outfile.py')
+	main(sys.argv[1], sys.argv[2], 'outfile.py')
